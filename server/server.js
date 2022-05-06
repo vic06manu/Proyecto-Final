20f1c82bab087db774d79646c168905290eb0f8c
Victor Martinez <18030550@itcelaya.edu.mx>
2022-05-06 08:39:52 -0500

    Carga de productos
    Craga de todos los productos
    Craga de producto por Id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,18 @@
 import express from "express";
+import products from "./data/Products.js";
+
 const app = express();
+
+// Carga de Productos
+app.get("/api/products", (req, res) =>{
+    res.json(products);
+});
+
+// Cragar de Productos por ID
+app.get("/api/products/:id", (req, res) =>{
+    const product = products.find((p) => p._id === req.params.id);
+    res.json(product);
+});
 
 app.get("/", (req, res) =>{
     res.send("Api run");
