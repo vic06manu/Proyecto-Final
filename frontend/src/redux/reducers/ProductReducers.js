f265eefc99b1f0fb8091c441a8de1741d747bea0
Victor Martinez <18030550@itcelaya.edu.mx>
2022-05-08 20:35:09 -0500

    Agregacion a un sigle product
    Agregamos el control de errores de carga  a la pantalla de un solo producto

--- frontend/src/redux/reducers/ProductReducers.js
+++ frontend/src/redux/reducers/ProductReducers.js
@@ -2,8 +2,12 @@ import {
     PRODUCT_LIST_FAIL,
     PRODUCT_LIST_REQUEST,
     PRODUCT_LIST_SUCCESS,
+    PRODUCT_DETAILS_FAIL,
+    PRODUCT_DETAILS_REQUEST,
+    PRODUCT_DETAILS_SUCCESS
 } from '../constants/ProductConstants';
 
+//PRODUCTS
 export const productListReducer = (state = {products:[]}, action)=>{
     switch (action.type) {
         case PRODUCT_LIST_REQUEST:
@@ -11,6 +15,22 @@ export const productListReducer = (state = {products:[]}, action)=>{
         case PRODUCT_LIST_SUCCESS:
             return {loading: false, products: action.payload};
         case PRODUCT_LIST_FAIL:
+            return {loading: false, error: action.payload};
+        default:
+            return state;
+    }
+};
+//PRODUCTS DETAIL
+export const productDetailsReducer = (
+    state = {product:{reviews:[]}},
+    action
+) => {
+    switch (action.type) {
+        case PRODUCT_DETAILS_REQUEST:
+            return {...state, loading: true};
+        case PRODUCT_DETAILS_SUCCESS:
+            return {loading: false, product: action.payload};
+        case PRODUCT_DETAILS_FAIL:
             return {loading: false, error: action.payload};
         default:
             return state;
