514c0c1dc9f76fc131d9409f4365df2bbee3eaee
Victor <18030550@itcelaya.edu.mx>
2022-05-24 16:51:59 -0500

    Login de Redux
    Se reealiza el login de redux para el manejo de informacion

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -3,21 +3,28 @@ import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import {productListReducer, productDetailsReducer} from './reducers/ProductReducers';
 import {cartReducer} from './reducers/CartReducers';
+import { userLoginReducer } from './reducers/userReducers';
 
 const reducer = combineReducers({
     productList : productListReducer,
     productDetails : productDetailsReducer,
     cart: cartReducer,
+    userLogin: userLoginReducer,
 });
 
 const cartItemsFromLocalStorage = localStorage.getItem('cartItems')
     ? JSON.parse(localStorage.getItem('cartItems'))
     : [];
 
+const userInfoFromLocalStorage = localStorage.getItem('userInfo')
+    ? JSON.parse(localStorage.getItem('userInfo'))
+    : null;
+
 const initialState = {
     cart:{
         cartItems: cartItemsFromLocalStorage,
-    }
+    },
+    userLogin: {userInfo: userInfoFromLocalStorage},
 };
 
 const middleware = [thunk];
