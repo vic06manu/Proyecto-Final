e5a6676371a707e04b264948a0533f8046a12aa9
Victor <18030550@itcelaya.edu.mx>
2022-05-24 21:32:09 -0500

    Detalles de Usuario
    Detalles del perfil del ususario

--- frontend/src/components/profileComponents/ProfileTabs.js
+++ frontend/src/components/profileComponents/ProfileTabs.js
@@ -1,32 +1,78 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import Toast from './../LoadingError/Toast';
+import Message from "../LoadingError/Error";
+import Loading from "../LoadingError/Loading";
 
 const ProfileTabs = () => {
+  const [name, setName  ] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const toastId = React.useRef(null)
+
+  const Toastobjects = {
+    pauseOnFocusLoss: false,
+    dragable: false,
+    pauseOnHover: false,
+    autoClose:2000
+
+  }
+
+  const dispatch = useDispatch();
+
+  const userDetails = useSelector((state) => state.userDetails);
+  const {loading, error, user} = userDetails;
+
+  useEffect(()=>{
+    if (user) {
+      setName(user.name);
+      setEmail(user.email);
+    }
+  },[dispatch, user ]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      toastId.current = toast.error("PAssword do not match", Toastobjects);
+    } else{
+      alert("Passwords match");
+    }
+  }
+
   return (
     <>
-      <form className="row  form-container">
+      <Toast/>
+      {error && <Message variant="alert-danger"> {error} </Message>}
+      {loading && <Loading/>}
+      <form className="row  form-container" onSubmit={submitHandler}>
         <div className="col-md-6">
           <div className="form">
             <label for="account-fn">UserName</label>
-            <input className="form-control" type="text" required />
+            <input className="form-control" type="text" required
+            value={name} onChange={(e)=> setName(e.target.value)}/>
           </div>
         </div>
 
         <div className="col-md-6">
           <div className="form">
             <label for="account-email">E-mail Address</label>
-            <input className="form-control" type="email" />
+            <input className="form-control" type="email" value={email} onChange={(e)=> setEmail(e.target.value)}/>
           </div>
         </div>
         <div className="col-md-6">
           <div className="form">
             <label for="account-pass">New Password</label>
-            <input className="form-control" type="password" />
+            <input className="form-control" type="password"
+            value={password} onChange={(e)=> setPassword(e.target.value)} />
           </div>
         </div>
         <div className="col-md-6">
           <div className="form">
             <label for="account-confirm-pass">Confirm Password</label>
-            <input className="form-control" type="password" />
+            <input className="form-control" type="password"
+            value={confirmPassword} onChange={(e)=> setConfirmPassword(e.target.value)}/>
           </div>
         </div>
         <button type="submit">Update Profile</button>
--- frontend/src/redux/actions/userActions.js
+++ frontend/src/redux/actions/userActions.js
@@ -1,4 +1,4 @@
-import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT, USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS } from "../constants/UserContants";
+import { USER_DETAILS_FAIL, USER_DETAILS_REQUEST, USER_DETAILS_SUCCESS, USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT, USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS } from "../constants/UserContants";
 import axios from "axios";
 
 export const login = (email, password) => async(dispatch) => {
@@ -48,6 +48,33 @@ export const register = (name, email, password) => async(dispatch) => {
             error.response && error.response.data.message
             ? error.response.data.message
             : error.message,
+        });
+    }
+};
+
+
+export const getUserDetails = (id) => async(dispatch,getState) => {
+    try {
+        dispatch({type: USER_DETAILS_REQUEST});
+        const {userLogin: {userInfo}} = getState();
+        const config = {
+            headers:{
+                Authorization: `Bearer ${userInfo.token}`
+            },
+        };
+        const {data} = await axios.get(`/api/users${id}` ,config);
+        dispatch({type: USER_DETAILS_SUCCESS, payload: data});
+
+    } catch (error) {
+        const message = error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message;
+            if (message==="not autorizado") {
+                dispatch(logout());
+            }
+        dispatch({
+            type: USER_DETAILS_FAIL,
+            payload:message,
         });
     }
 };
